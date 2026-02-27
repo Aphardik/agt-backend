ab21880113f9f7b9aa0d48dd2a6363109611a879
hardik170220 <panchalhardik1702@gmail.com>
2026-02-27 17:00:24 +0530

    book route code clean

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,30 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const bookController = require('../controllers/book.controller.js');
-
-// // const multer = require('multer');
-// // const path = require('path');
-
-// // const storage = multer.memoryStorage();
-
-// // const upload = multer({ storage: storage });
-// const upload = require('../config/multer');
-
-
-// const uploadFields = upload.fields([
-//     { name: 'frontImage', maxCount: 1 },
-//     { name: 'backImage', maxCount: 1 }
-// ]);
-
-// // router.get('/:id/image/:type', bookController.getBookImage);
-// router.get('/', bookController.getAllBooks);
-// router.get('/:id', bookController.getBookById);
-// router.post('/bulk', bookController.createMultipleBooks);
-// router.post('/', uploadFields, bookController.createBook);
-// router.put('/:id', uploadFields, bookController.updateBook);
-// router.delete('/:id', bookController.deleteBook);
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/book.controller.js');
